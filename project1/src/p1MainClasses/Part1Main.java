6c1543f228735971e15063da570a0cda456ab3d2
Cesar <cesar.justiniano@upr.edu>
2018-03-22 01:28:05 -0400

    updated main 1

--- project1/src/p1MainClasses/Part1Main.java
+++ project1/src/p1MainClasses/Part1Main.java
@@ -10,6 +10,7 @@ import mySetImplementations.P1_P2;
 import mySetImplementations.P3;
 import mySetImplementations.P4;
 import mySetImplementations.Set1;
+import mySetImplementations.Set2;
 
 public class Part1Main {
 
@@ -21,8 +22,7 @@ public class Part1Main {
 		String strategy;
 		DataReader dr = new DataReader();
 
-
-		Object [] data = dr.readDataFiles();
+		Object [][][] data = dr.readDataFiles();
 		
 
 		P1_P2<Integer> s1 = new P1_P2<Integer>("1");
@@ -30,7 +30,6 @@ public class Part1Main {
 		P3<Integer> s3 = new P3<Integer>("3");
 		P4<Integer> s4 = new P4<Integer>("4");
 		
-//		dr.readDataFiles();
 		
 		do{
 			System.out.println("My Set menu:");
@@ -46,25 +45,27 @@ public class Part1Main {
 			
 			switch(strategy){
 				case "":
-					System.out.println("Final set by P1: " + s1.intersectSets( (MySet<Integer>[]) data));
-					System.out.println("Final set by P2: " + s2.intersectSets( (MySet<Integer>[]) data));
-					System.out.println("Final set by P3: " + s3.intersectSets( (MySet<Integer>[]) data));
-					System.out.println("Final set by P4: " + s4.intersectSets( (MySet<Integer>[]) data));
+					
+//					System.out.println("Final set by P1: " + s1.intersectSets( (MySet<Integer>[]) data));
+//					System.out.println("Final set by P2: " + s2.intersectSets( (MySet<Integer>[]) data));
+//					System.out.println("Final set by P3: " + s3.intersectSets( (MySet<Integer>[]) data));
+//					System.out.println("Final set by P4: " + s4.intersectSets( (MySet<Integer>[]) data));
 					break;
 				case "1":
-					System.out.println("Final set by P1: " + s1.intersectSets( (MySet<Integer>[]) data));
+					Set1<Integer> union1 = toArray1(data);
+					System.out.println("Final set by P1: " + s1.intersectSets( (MySet<Integer>[]) union1.toArray()));
 					break;
 				case "2":
-
-					System.out.println("Final set by P2: " + s2.intersectSets( (MySet<Integer>[]) data));
+					Set2<Integer> union2 = toArray2(data);
+					System.out.println("Final set by P2: " + s2.intersectSets( (MySet<Integer>[]) union2.toArray()));
 					break;
 				case "3":
-
-					System.out.println("Final set by P3: " + s3.intersectSets( (MySet<Integer>[]) data));
+					Set2<Integer> union3 = toArray2(data);
+					System.out.println("Final set by P3: " + s3.intersectSets( (MySet<Integer>[]) union3.toArray()));
 					break;
 				case "4":
-					
-					System.out.println("Final set by P4: " + s4.intersectSets( (MySet<Integer>[]) data));
+					Set2<Integer> union4 = toArray2(data);
+					System.out.println("Final set by P4: " + s4.intersectSets( (MySet<Integer>[]) union4.toArray()));
 					break;
 				case "5":
 					System.out.println("Thank you for using this program.");
@@ -77,7 +78,26 @@ public class Part1Main {
 		
 		scan.close();
 	}
+	
 	public String Allresults(String res) {
 		return res;
+	}
+	
+	public static Set1<Integer> toArray1(Object [][] data){
+		Set1<Integer> set1 = new Set1<Integer>();
+		for(int i=0;i<data.length;i++)
+			for(int j=0;j<data.length;j++)
+				set1.add((Integer) data[i][j]);
+		
+		return set1;
+	}
+	
+	public static Set2<Integer> toArray2(Object [][] data){
+		Set2<Integer> set2 = new Set2<Integer>();
+		for(int i=0;i<data.length;i++)
+			for(int j=0;j<data.length;j++)
+				set2.add((Integer) data[i][j]);
+		
+		return set2;
 	}
 }
